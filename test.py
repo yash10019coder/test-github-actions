dd7f4dab430f63dbae7a9c1017f1a588825e42c4
Praneeth <praneethg2001@gmail.com>
2021-03-04 01:50:51 +0530

    testing codeowner change

--- test.py
+++ test.py
@@ -1,5 +1,20 @@
-import sys
+import re
 
-print("changelog 1")
-print("changelog 2")
-print("changelog 3")
+my_file = open("CODE", "+r")
+
+content = my_file.read()
+
+if re.search('ran',content):
+  content = re.sub('ran', 'gp201', content)
+else:
+  content = re.sub('gp201', 'ran', content)
+
+my_file.seek(0)
+my_file.write(content)
+my_file.truncate()
+
+my_file.seek(0)
+
+print(my_file.read())
+
+my_file.close()

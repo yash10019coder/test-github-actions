ac77fa17b64fd3a1a1c810b12b0f80adf8c0bb66
Praneeth <praneethg2001@gmail.com>
2021-03-17 22:31:06 +0530

    moving token to secrets0.5

--- cla_check_2.py
+++ cla_check_2.py
@@ -13,7 +13,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1naQC7iEfnro5iOjTFEn7iPCxNMPaPa4YnIddjT5CTM8'
 SAMPLE_RANGE_NAME = 'Usernames'
-TOKEN = ''
+TOKEN = os.environ['SHEETS_TOKEN']
 
 def getValues():
     creds = None
@@ -28,8 +28,7 @@ def getValues():
 
 
 def main():
-    TOKEN = sys.argv[1]
-    prAuthor = sys.argv[2]
+    prAuthor = sys.argv[1]
 
     print(TOKEN)
     values = getValues()

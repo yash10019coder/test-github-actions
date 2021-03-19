f0aeb3af78c520ee80846d9a565033b154f0c555
Praneeth <praneethg2001@gmail.com>
2021-03-19 13:41:25 +0530

    fixed issues

--- actions/index.js
+++ actions/index.js
@@ -1,44 +1,42 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const {google} = require('googleapis');
+const { google } = require('googleapis');
 
-var token = process.env.SHEETS_TOKEN;
-var credentials = process.env.SHEETS_CRED;
-authorize(JSON.parse(credentials), listMajors);
+const TOKEN = process.env.SHEETS_TOKEN;
+const CREDENTIALS = process.env.SHEETS_CRED;
+const SPREADSHEET_ID = '1naQC7iEfnro5iOjTFEn7iPCxNMPaPa4YnIddjT5CTM8';
+const RANGE = 'Usernames';
+
+authorize(listMajors);
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
  * given callback function.
- * @param {Object} credentials The authorization client credentials.
+ * @param {Object} CREDENTIALS The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
- function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.web;
+function authorize(callback) {
+  const { client_secret, client_id, redirect_uris } = CREDENTIALS.web;
   const oAuth2Client = new google.auth.OAuth2(
     client_id, client_secret, redirect_uris[0]);
-  oAuth2Client.setCredentials(JSON.parse(token));
+  oAuth2Client.setCredentials(JSON.parse(TOKEN));
   callback(oAuth2Client);
 }
 
 /**
  * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
  */
- function listMajors(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
+function listMajors(auth) {
+  const sheets = google.sheets({ version: 'v4', auth });
   sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
+    spreadsheetId: SPREADSHEET_ID,
+    range: RANGE,
   }, (err, res) => {
     if (err) return console.log('The API returned an error: ' + err);
     const rows = res.data.values;
     if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
-      });
+      console.log(rows);
     } else {
       console.log('No data found.');
     }

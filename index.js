99b7a2ee20535663740a1673a40abf6145481ed4
Praneeth <praneethg2001@gmail.com>
2021-04-02 16:27:35 +0530

    testing

--- index.js
+++ index.js
@@ -1,92 +1,115 @@
-const fs = require('fs');
-const readline = require('readline');
-const {google} = require('googleapis');
-
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
-
-// Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
-  // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
-});
+// Copyright 2021 The Oppia Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 /**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- * @param {Object} credentials The authorization client credentials.
- * @param {function} callback The callback to call with the authorized client.
+ * @fileoverview File to check if PR Author has signed the CLA
  */
-function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
 
-  // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
-    oAuth2Client.setCredentials(JSON.parse(token));
-    callback(oAuth2Client);
-  });
-}
-
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client, callback) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      // Store the token to disk for later program executions
-      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-        if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
-      });
-      callback(oAuth2Client);
-    });
-  });
-}
-
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function listMajors(auth) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.get({
-    spreadsheetId: '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms',
-    range: 'Class Data!A2:E',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.values;
-    if (rows.length) {
-      console.log('Name, Major:');
-      // Print columns A and E, which correspond to indices 0 and 4.
-      rows.map((row) => {
-        console.log(`${row[0]}, ${row[4]}`);
-      });
-    } else {
-      console.log('No data found.');
-    }
-  });
-}
+ const core = require('@actions/core');
+ const { context } = require('@actions/github');
+ const { execSync } = require('child_process');
+ const { google } = require('googleapis');
+ const RANGE = 'Usernames';
+ 
+ /**
+  * Create an OAuth2 client with the given credentials, and then execute the
+  * given claCheck function.
+  */
+ const authorize = function() {
+   const SHEETS_TOKEN = JSON.parse(process.env.SHEETS_TOKEN);
+   const CREDENTIALS = JSON.parse(process.env.SHEETS_CRED);
+   try {
+     // eslint-disable-next-line camelcase
+     const { client_secret, client_id, redirect_uris } = CREDENTIALS.installed;
+     const oAuth2Client = new google.auth.OAuth2(
+       client_id,
+       client_secret,
+       redirect_uris[0]
+     );
+     oAuth2Client.setCredentials(SHEETS_TOKEN);
+     return oAuth2Client;
+   } catch (err){
+     core.setFailed('Auth failure: ' + err);
+   }
+ };
+ 
+ const generateOutput = (hasClaSigned) => {
+   const LINK_RESULT = 'here'.link(
+     'https://github.com/oppia/oppia/wiki/' +
+     'Contributing-code-to-Oppia#setting-things-up'
+   );
+   const PR_NUMBER = context.payload.pull_request.number;
+   const PR_AUTHOR = context.payload.pull_request.user.login;
+ 
+   let comment = '';
+   let cmd = '';
+   if (!hasClaSigned) {
+     comment = ('Hi! @' +
+         PR_AUTHOR +
+         ' Welcome to Oppia! Please could you ' +
+         'follow the instructions ' + LINK_RESULT +
+         " to get started? You'll need to do " +
+         'this before we can accept your PR. Thanks!');
+     cmd = 'gh pr comment ' + PR_NUMBER + ' --body "' + comment + '"';
+     core.info(`${cmd}`);
+     execSync(cmd);
+     core.setFailed(PR_AUTHOR + ' has not signed the CLA');
+   } else {
+     core.info(`${PR_AUTHOR} has signed the CLA`);
+   }
+ };
+ 
+ 
+ /**
+  * Checks if the PR Author has signed the CLA Sheet.
+  * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+  */
+ const checkSheet = async (auth) => {
+   const PR_AUTHOR = context.payload.pull_request.user.login;
+   const SPREADSHEET_ID = process.env.SPREADSHEET_ID;
+   const sheets = google.sheets({ version: 'v4', auth });
+   await sheets.spreadsheets.values.get(
+     {
+       spreadsheetId: SPREADSHEET_ID,
+       range: RANGE,
+     },
+     async (err, res) => {
+       if (err) {
+         core.setFailed('The API returned an error: ' + err);
+       }
+       const rows = res.data.values;
+       const flatRows = [].concat.apply([], rows);
+       if (!rows || rows.length === 0) {
+         core.setFailed('No data found.');
+       } else {
+         core.info(`Checking if ${PR_AUTHOR} has signed the CLA`);
+         const hasUserSignedCla = flatRows.includes(PR_AUTHOR);
+         generateOutput(hasUserSignedCla);
+       }
+     }
+   );
+ };
+ 
+ const claCheckGithubAction = () => {
+   // Authorize a client with the loaded credentials.
+   const auth = authorize();
+ 
+   // Call the sheets API with the authorized client.
+   checkSheet(auth);
+ };
+ 
+ module.exports = {
+   claCheckGithubAction,
+ };
+ 
